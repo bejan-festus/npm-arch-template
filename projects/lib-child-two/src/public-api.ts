5f904564d932c96547e2f7b596940434f10468c8
bejan festus <bejan@tangotech.co.in>
2024-02-01 12:51:37 +0530

    projects removed

--- projects/lib-child-two/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of lib-child-two
- */
-
-export * from './lib/lib-child-two.service';
-export * from './lib/lib-child-two.component';
-export * from './lib/lib-child-two.module';
